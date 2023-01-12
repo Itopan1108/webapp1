4f1d4c147e436cc12cb1ec9b7230f9412b796595
hyamakawa <h-yamakawa@esm.co.jp>
2023-01-12 10:23:04 +0900

    POST_URLをもとに戻した

--- script/challenger_study_record/src/common/constants.js
+++ script/challenger_study_record/src/common/constants.js
@@ -1,8 +1,8 @@
 //Slack関連情報
 export const SLACK_INFO = {
   // メッセージ投稿情報
-  // POST_URL: "https://hooks.slack.com/services/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",
-  POST_URL: "https://hooks.slack.com/services/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", // テスト用（マイチャンネル）
+  POST_URL: "https://hooks.slack.com/services/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",
+  // POST_URL: "https://hooks.slack.com/services/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", // テスト用（マイチャンネル）
   // メッセージ取得情報
   ESM_FD_TOKEN: "xoxp-3963306992470-4007513038819-4119166819828-1fcf363207500ce1e830678cbecfaf40",
   NOTIFY_MODERN_STUDY_CHANNEL_ID: "C042NE3A2NS",
