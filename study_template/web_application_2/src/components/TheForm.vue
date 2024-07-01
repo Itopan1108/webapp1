aacfc13d9a83a96f27af791f49fb7c45ff2938a5
“Itopan1108” <tito@esm.co.jp>
2024-07-01 20:44:27 +0900

    あらためて初回プッシュ!

--- study_template/web_application_2/src/components/TheForm.vue
+++ study_template/web_application_2/src/components/TheForm.vue
@@ -1,64 +1,64 @@
 <template>
-    <v-card>
-      <v-card-title>
-        <span class="headline">登録／修正</span>
-      </v-card-title>
-      <v-card-text>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="6" md="4">
-              <v-avatar color="indigo">
-                <v-icon dark>mdi-account-circle</v-icon>
-              </v-avatar>
-            </v-col>
-            <v-col cols="12">
-              <v-label for="book-name" class="label_book">書籍名</v-label>
-              <v-text-field label="書籍名*" id="book-name" v-model="returnData.book" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-label for="human-name" class="label_human">所有者名</v-label>
-              <v-text-field label="所有者*" id="human-name" v-model="returnData.human" required></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-label for="address-name" class="label_address">所在</v-label>
-              <v-select :items="['福井', '石川', '富山']" label="所在*" id="address-name" v-model="returnData.address" required></v-select>
-            </v-col>
-            <v-col cols="12">
-              <v-label for="url-name" class="label_url">ＵＲＬ</v-label>
-              <v-text-field label="ＵＲＬ" id="url-name" v-model="returnData.url"></v-text-field>
-            </v-col>
-            <v-col cols="12">
-              <v-label for="comment-name" class="label_comment">ＵＲＬ</v-label>
-              <v-text-field label="コメント" id="comment-name" v-model="returnData.comment"></v-text-field>
-            </v-col>
-          </v-row>
-        </v-container>
-       </v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn text color="success" @click="submit">
-        登録
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+  <v-card>
+    <v-card-title>
+      <span class="headline">登録／修正</span>
+    </v-card-title>
+    <v-card-text>
+      <v-container>
+        <v-row>
+          <v-col cols="12" sm="6" md="4">
+            <v-avatar color="indigo">
+              <v-icon dark>mdi-account-circle</v-icon>
+            </v-avatar>
+          </v-col>
+          <v-col cols="12">
+            <v-label for="book-name" class="label_book">書籍名</v-label>
+            <v-text-field label="書籍名*" id="book-name" v-model="returnData.book" required></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-label for="category-name" class="label_category">ジャンル</v-label>
+            <v-select :items="['漫画', '小説', 'ビジネス', '趣味']" label="ジャンル*" id="category-name" v-model="returnData.category" required></v-select>
+          </v-col>
+          <v-col cols="12">
+            <v-label for="buyer-name" class="label_buyer">買った人</v-label>
+            <v-text-field label="買った人*" id="buyer-name" v-model="returnData.buyer" required></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-label for="boughtday" class="label_boughtday">ＵＲＬ</v-label>
+            <v-text-field label="買った日" id="boughtday" v-model="returnData.boughtday"></v-text-field>
+          </v-col>
+          <v-col cols="12">
+            <v-label for="comment-name" class="label_comment">コメント</v-label>
+            <v-text-field label="コメント" id="comment-name" v-model="returnData.comment"></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn text color="success" @click="submit">
+      登録
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
 export default {
   props: {
     book: { type: String },
-    human: { type: String },
-    address: { type: String },
-    url: { type: String },
+    category: { type: String },
+    buyer: { type: String },
+    boughtday: { type: String },
     comment: { type: String }
   },
   data () {
     return {
       returnData: {
         book: this.book,
-        human: this.human,
-        address: this.address,
-        url: this.url,
+        category: this.category,
+        buyer: this.buyer,
+        boughtday: this.boughtday,
         comment: this.comment
       }
     }
--- study_template/web_application_2/src/components/TheList.vue
+++ study_template/web_application_2/src/components/TheList.vue
@@ -1,30 +1,19 @@
 <template>
   <div>
-   <table border="1" bordercolor="black" id="table">
+    <table border="1" bordercolor="black" id="table">
     <tr bgcolor="gray">
-     <th >書籍名</th>
-     <th >所在</th>
-     <th >所有者</th>
-     <th colspan="3">貸出状況</th>
-     <th colspan="2">予約状況</th>
-     <th >修正</th>
-     <th >削除</th>
+      <th >書籍名</th>
+      <th >ジャンル</th>
+      <th >買った人</th>
+      <th >買った日</th>
     </tr>
     <tr>
-     <td width="30%">面白い本（仮）</td>
-     <td width="5%">福井（仮）</td>
-     <td width="10%">いとぱん（仮）</td>
-     <td width="10%"><button type="button" id="btn4">貸出</button>
-      <button type="button" id="btn5">返却</button></td>
-     <td width="5%">伊藤（仮）</td>
-     <td width="10%">2024/02/14（仮）</td>
-     <td width="10%"><button type="button" id="btn6">予約</button>
-      <button type="button" id="btn7">予中</button></td>
-     <td width="10%">予約できるよ（仮）</td>
-     <td width="5%"><button type="button" id="btn8">修正</button></td>
-     <td width="5%"><button type="button" id="btn9">削除</button></td>
+      <td width="30%">面白い本（仮）</td>
+      <td width="5%">まんが（仮）</td>
+      <td width="10%">いとぱん（仮）</td>
+      <td width="10%">６月２４日（仮）</td>
     </tr>
-   </table>
+    </table>
   </div>
 </template>
 <script>
@@ -38,10 +27,10 @@ export default {
 }
 </script>
 <style scoped>
- #table {
+#table {
   width: 1600px;
   table-layout: auto;}
- #btn4,#btn6,#btn8 {
+#btn4,#btn6,#btn8 {
   width: 50px;
   background-color: greenyellow;
   text-align: center;
@@ -51,7 +40,7 @@ export default {
   margin: 10px;
   border-radius: 3px;
   }
- #btn5,#btn7,#btn9 {
+#btn5,#btn7,#btn9 {
   width: 50px;
   background-color: orange;
   text-align: center;
